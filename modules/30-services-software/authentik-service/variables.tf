8b0a1a8e4be9f46a45fb887ec500c06358e091ae
Jeroen Vijgen <jjvijgen@gmail.com>
2025-08-14 13:17:23 +0000

    Upgrade authentik

--- modules/30-services-software/authentik-service/variables.tf
+++ modules/30-services-software/authentik-service/variables.tf
@@ -1,12 +1,12 @@
 
 variable "image_tag" {
-  description = "The tag for the authentik container image. Default: 2025.6.3"
+  description = "The tag for the authentik container image. Default: 2025.6.4"
   type        = string
-  default     = "2025.6.3"
+  default     = "2025.6.4"
 }
 
 variable "redis_image_tag" {
-  description = "The tag for the redis container image. Default: 2025.6.3"
+  description = "The tag for the redis container image. Default: alpine"
   type        = string
   default     = "alpine"
 }
