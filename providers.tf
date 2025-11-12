d403f4f3e83b3c4356896d3a91dc2a63467adabd
Jeroen Vijgen <jjvijgen@gmail.com>
2025-11-12 15:35:10 +0000

    Fix newline after argument according to Duck.ai

--- providers.tf
+++ providers.tf
@@ -4,10 +4,12 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 3.6.0"
     }
+
     random = {
       source  = "hashicorp/random"
       version = "~> 3.5.1"
     }
+
     dotenv = {
       source  = "germanbrew/dotenv"
       version = "1.2.5"
@@ -17,5 +19,4 @@ terraform {
 
 provider "docker" {
   host = provider::dotenv::get_by_key("DOCKER_SOCK", "${path.module}/.env")
-
 }
