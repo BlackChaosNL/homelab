1a626e4d449d974bb8ddae55a0aae2b669523ea3
Jeroen Vijgen <jjvijgen@gmail.com>
2026-04-02 13:43:03 +0000

    hotfix: expose TCP and UDP listeners for satisfactory, don't pipe through caddy yet

--- modules/20-services-entertainment/satisfactory-service/main.tf
+++ modules/20-services-entertainment/satisfactory-service/main.tf
@@ -24,6 +24,23 @@ module "satisfactory" {
   networks       = var.networks
   restart_policy = "always"
   memory_limit   = 16000 // 16Gb
+  ports = [
+    {
+      internal = 7777
+      external = 7777
+      protocol = "tcp"
+    },
+    {
+      internal = 7777
+      external = 7777
+      protocol = "udp"
+    },
+    {
+      internal = 8888
+      external = 8888
+      protocol = "tcp"
+    }
+  ]
   volumes = [
     {
       host_path      = "${var.volume_path}/${local.container_name}/config"
@@ -44,6 +61,5 @@ output "service_definition" {
     name         = local.container_name
     primary_port = local.satisfactory_internal_port
     endpoint     = "http://${local.container_name}:${local.satisfactory_internal_port}"
-    subdomains   = ["satisfactory"]
   }
 }
