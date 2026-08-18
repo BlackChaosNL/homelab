b710834069a3b948459de398e02c58646281370c
Jeroen Vijgen <jjvijgen@gmail.com>
2026-08-18 00:14:09 +0000

    HOTFIX: Persist CODER_OAUTH2_GITHUB_DEFAULT_PROVIDER_ENABLE into configs

--- modules/30-services-software/coder-service/main.tf
+++ modules/30-services-software/coder-service/main.tf
@@ -33,20 +33,21 @@ locals {
   ]
 
   coder_env_vars = {
-    CODER_PG_CONNECTION_URL     = "postgresql://${provider::dotenv::get_by_key("POSTGRES_USER", local.env_file)}:${provider::dotenv::get_by_key("POSTGRES_PASSWORD", local.env_file)}@coder-postgres/${provider::dotenv::get_by_key("POSTGRES_DB", local.env_file)}?sslmode=disable"
-    CODER_HTTP_ADDRESS          = provider::dotenv::get_by_key("CODER_HTTP_ADDRESS", local.env_file)
-    CODER_ACCESS_URL            = provider::dotenv::get_by_key("CODER_ACCESS_URL", local.env_file)
-    CODER_PROXY_TRUSTED_HEADERS = provider::dotenv::get_by_key("CODER_PROXY_TRUSTED_HEADERS", local.env_file)
-    CODER_PROXY_TRUSTED_ORIGINS = provider::dotenv::get_by_key("CODER_PROXY_TRUSTED_ORIGINS", local.env_file)
-    CODER_DISABLE_PASSWORD_AUTH = provider::dotenv::get_by_key("CODER_DISABLE_PASSWORD_AUTH", local.env_file)
-    DOCKER_USER                 = provider::dotenv::get_by_key("DOCKER_USER", local.env_file)
-    CODER_OIDC_ISSUER_URL       = provider::dotenv::get_by_key("CODER_OIDC_ISSUER_URL", local.env_file)
-    CODER_OIDC_EMAIL_DOMAIN     = provider::dotenv::get_by_key("CODER_OIDC_EMAIL_DOMAIN", local.env_file)
-    CODER_OIDC_CLIENT_ID        = provider::dotenv::get_by_key("CODER_OIDC_CLIENT_ID", local.env_file)
-    CODER_OIDC_CLIENT_SECRET    = provider::dotenv::get_by_key("CODER_OIDC_CLIENT_SECRET", local.env_file)
-    CODER_OIDC_SCOPES           = provider::dotenv::get_by_key("CODER_OIDC_SCOPES", local.env_file)
-    CODER_OIDC_SIGN_IN_TEXT     = provider::dotenv::get_by_key("CODER_OIDC_SIGN_IN_TEXT", local.env_file)
-    CODER_OIDC_ICON_URL         = provider::dotenv::get_by_key("CODER_OIDC_ICON_URL", local.env_file)
+    CODER_PG_CONNECTION_URL                     = "postgresql://${provider::dotenv::get_by_key("POSTGRES_USER", local.env_file)}:${provider::dotenv::get_by_key("POSTGRES_PASSWORD", local.env_file)}@coder-postgres/${provider::dotenv::get_by_key("POSTGRES_DB", local.env_file)}?sslmode=disable"
+    CODER_HTTP_ADDRESS                          = provider::dotenv::get_by_key("CODER_HTTP_ADDRESS", local.env_file)
+    CODER_ACCESS_URL                            = provider::dotenv::get_by_key("CODER_ACCESS_URL", local.env_file)
+    CODER_PROXY_TRUSTED_HEADERS                 = provider::dotenv::get_by_key("CODER_PROXY_TRUSTED_HEADERS", local.env_file)
+    CODER_PROXY_TRUSTED_ORIGINS                 = provider::dotenv::get_by_key("CODER_PROXY_TRUSTED_ORIGINS", local.env_file)
+    CODER_DISABLE_PASSWORD_AUTH                 = provider::dotenv::get_by_key("CODER_DISABLE_PASSWORD_AUTH", local.env_file)
+    CODER_OAUTH2_GITHUB_DEFAULT_PROVIDER_ENABLE = provider::dotenv::get_by_key("CODER_OAUTH2_GITHUB_DEFAULT_PROVIDER_ENABLE", local.env_file)
+    DOCKER_USER                                 = provider::dotenv::get_by_key("DOCKER_USER", local.env_file)
+    CODER_OIDC_ISSUER_URL                       = provider::dotenv::get_by_key("CODER_OIDC_ISSUER_URL", local.env_file)
+    CODER_OIDC_EMAIL_DOMAIN                     = provider::dotenv::get_by_key("CODER_OIDC_EMAIL_DOMAIN", local.env_file)
+    CODER_OIDC_CLIENT_ID                        = provider::dotenv::get_by_key("CODER_OIDC_CLIENT_ID", local.env_file)
+    CODER_OIDC_CLIENT_SECRET                    = provider::dotenv::get_by_key("CODER_OIDC_CLIENT_SECRET", local.env_file)
+    CODER_OIDC_SCOPES                           = provider::dotenv::get_by_key("CODER_OIDC_SCOPES", local.env_file)
+    CODER_OIDC_SIGN_IN_TEXT                     = provider::dotenv::get_by_key("CODER_OIDC_SIGN_IN_TEXT", local.env_file)
+    CODER_OIDC_ICON_URL                         = provider::dotenv::get_by_key("CODER_OIDC_ICON_URL", local.env_file)
   }
 
   postgres_env_vars = {
